b4a6b15414a6107bd8ba5e9c789cda2a76c164fa
Khwilo Kabaka <khwilowatai@gmail.com>
2020-05-06 14:22:25 +0300

    Refactor component to use the filter logic

--- src/containers/Cards/Cards.js
+++ src/containers/Cards/Cards.js
@@ -1,69 +1,13 @@
 import React from 'react';
 
 import Card from '../../components/Card/Card';
+import createFilterObject from '../../utils/createFilterObject';
+import filterListByMultipleValues from '../../utils/filterListByMultipleValues';
 import './Cards.css';
 
 const Cards = ({ data, filters }) => {
-  const filterObj = {
-    role: '',
-    level: '',
-    contract: '',
-    location: '',
-    languages: [],
-    tools: [],
-  };
-
-  filters.forEach((value) => {
-    if (value === 'Frontend' || value === 'Fullstack' || value === 'Backend') {
-      filterObj['role'] = value;
-    }
-
-    if (value === 'Senior' || value === 'Midweight' || value === 'Junior') {
-      filterObj.level = value;
-    } else if (
-      value === 'Full Time' ||
-      value === 'Part Time' ||
-      value === 'Contract'
-    ) {
-      filterObj['contract'] = value;
-    } else if (
-      value === 'USA Only' ||
-      value === 'Remote' ||
-      value === 'Worldwide' ||
-      value === 'UK Only'
-    ) {
-      filterObj['location'] = value;
-    } else if (
-      value === 'HTML' ||
-      value === 'CSS' ||
-      value === 'JavaScript' ||
-      value === 'Python'
-    ) {
-      filterObj['languages'] = [...filterObj['languages'], value];
-    } else if (
-      value === 'React' ||
-      value === 'Sass' ||
-      value === 'Ruby' ||
-      value === 'RoR' ||
-      value === 'Vue' ||
-      value === 'Django'
-    ) {
-      filterObj['tools'] = [...filterObj['tools'], value];
-    }
-  });
-
-  const filteredJobs =
-    filters.length !== 0
-      ? data.filter((job) => {
-          let result = false;
-          for (let key in job) {
-            if (filterObj[key] && job[key] === filterObj[key]) {
-              result = true;
-            }
-          }
-          return result;
-        })
-      : data;
+  const filterObject = createFilterObject(filters);
+  const filteredJobs = filterListByMultipleValues(data, filterObject);
 
   const cardsView = filteredJobs.map((job) => {
     return (
